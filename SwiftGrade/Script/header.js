7eb911a42ade715eaa8fb8f9d25998e18b83c9ca
Mstxz <mewadeeseeda@gmail.com>
2023-11-10 20:00:08 +0700

    fixed dropdown

--- SwiftGrade/Script/header.js
+++ SwiftGrade/Script/header.js
@@ -1,16 +1,15 @@
-function DropDown() {
-    document.getElementById("Drop").classList.toggle("show");
-  }
-
-window.onclick = function(event) {
-    if (!event.target.matches('.hamburger')) {
-      var dropdowns = document.getElementsByClassName("dropdown-c");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+document.addEventListener('click', function(event) {
+  var dropdowns = document.getElementsByClassName('dropdown-c');
+  for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show') && !openDropdown.parentNode.contains(event.target)) {
+      openDropdown.classList.remove('show');
     }
   }
+});
+
+document.querySelectorAll('.hamburger').forEach(function (dropdown) {
+  dropdown.addEventListener('click', function () {
+    this.querySelector('.dropdown-c').classList.toggle('show');
+  });
+});
