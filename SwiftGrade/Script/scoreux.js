84b76f22137e92fd64fca00d72502458ca7b95b1
Mstxz <mewadeeseeda@gmail.com>
2023-11-20 00:39:13 +0700

    add example content

--- SwiftGrade/Script/scoreux.js
+++ SwiftGrade/Script/scoreux.js
@@ -10,7 +10,15 @@ var optionData = {
         ["Health and Physical Education", "50","50","100","4"],
         ["Art", "50","50","100","4"],
         ["English Fundamental", "50","50","100","4"],
-      // Add more rows for Option 1 if needed
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["Mathematics MLP", "50","50","100","4"],
+        ["Physics", "50","50","100","4"],
+        ["Chemistry", "50","50","100","4"],
+        ["Biology", "50","50","100","4"],
+        ["Social Supplemental", "50","50","100","4"],
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["English Supplemental", "50","50","100","4"],
+        ["English MLP", "50","50","100","4"],
     ],
     opt2: [
         ["Name", "Midterm","Final","Score","GPA"],
@@ -23,9 +31,37 @@ var optionData = {
         ["Health and Physical Education", "50","50","100","4"],
         ["Art", "50","50","100","4"],
         ["English Fundamental", "50","50","100","4"],
-      // Add more rows for Option 2 if needed
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["Mathematics MLP", "50","50","100","4"],
+        ["Physics", "50","50","100","4"],
+        ["Chemistry", "50","50","100","4"],
+        ["Biology", "50","50","100","4"],
+        ["Social Supplemental", "50","50","100","4"],
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["English Supplemental", "50","50","100","4"],
+        ["English MLP", "50","50","100","4"],
     ],
-    // Add more options as needed
+    opt3: [
+        ["Name", "Midterm","Final","Score","GPA"],
+        ["Thai Fundamental", "50","50","100","4"],
+        ["Mathematics Fundamental", "50","50","100","4"],
+        ["Science Fundamental", "50","50","100","4"],
+        ["Computer", "50","50","100","4"],
+        ["Social Fundamental", "50","50","100","4"],
+        ["History", "50","50","100","4"],
+        ["Health and Physical Education", "50","50","100","4"],
+        ["Art", "50","50","100","4"],
+        ["English Fundamental", "50","50","100","4"],
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["Physics", "50","50","100","4"],
+        ["Chemistry", "50","50","100","4"],
+        ["Biology", "50","50","100","4"],
+        ["Artificial Intelligence Basics","50","50","100","4"]
+        ["GCSS","50","50","100","4"]
+        ["Mathematics Supplemental", "50","50","100","4"],
+        ["English Supplemental", "50","50","100","4"],
+        ["English MLP", "50","50","100","4"],
+    ],
     default: [
         ["Name", "Midterm","Final","Score","GPA"],
     ]
